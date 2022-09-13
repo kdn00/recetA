541f6632f9948917a9ffe0aacd8bbfa80ba02648
user <user@DESKTOP-RF3H2VG>
2022-09-13 14:52:18 +0900

    updatecon 주석 추가

--- maven/src/main/java/com/recetA/controller/UpdateCon.java
+++ maven/src/main/java/com/recetA/controller/UpdateCon.java
@@ -6,15 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UpdateCon
- */
+
 public class UpdateCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+//비밀번호, 전화번호
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
